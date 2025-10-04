46247e0bdacf0d5935b4ce19097b558f7fc4f713
Mayank <155724605+Mayankjain995@users.noreply.github.com>
2025-10-04 21:34:26 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // ✅ Add this line
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
